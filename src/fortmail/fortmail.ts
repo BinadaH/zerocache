c17a85246fa7c73c947c2b9fb19282018f2e979e
renqto <renato.imparato05@gmail.com>
2025-12-26 23:44:16 +0100

    fortmail start

--- src/fortmail/fortmail.ts
+++ src/fortmail/fortmail.ts
@@ -1,7 +1,21 @@
 import fortmail from './fortmail.html?raw'
+import fortmailLogin from './fortmail-login.html?raw'
 import fortmailCSS from "./fortmail.css?inline"
+import navBar from "./fortmail-nav.html?raw"
+import navBarCSS from "./fortmail-nav.css?inline"
+import {router} from "../router"
 
-export function load(viewport : HTMLDivElement, path : string){
-    viewport.innerHTML = fortmail
-    viewport.innerHTML += `<style>${fortmailCSS}</style>`
+export function load(viewport: HTMLDivElement, path: string){
+    const site = navBar + 
+                fortmailLogin +
+                `<style>${navBarCSS}${fortmailCSS}</style>`
+    viewport.innerHTML = site
+    
+    document.querySelector<HTMLButtonElement>("#login")!.onclick = () => {
+        router.navigateTo("fartmail.com")
+    }
+
+    document.querySelector<HTMLButtonElement>("#login2")!.onclick = () => {
+        router.navigateTo("fartmail.com")
+    }
 }
